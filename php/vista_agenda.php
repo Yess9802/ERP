721b4c377d929459a30e99238779929f88638370
yesenia <yesenia>
2019-11-05 14:43:47 -0600

    versiones importantes

---
+++ php/vista_agenda.php
@@ -0,0 +1,18 @@
+<section>
+	<form action="" method="post">
+		Empleado:
+		<select name="empleado">
+			
+		</select>
+		<br>
+		Fecha: <input type="date" name="fecha">
+		<br>
+		Hora: <input type="time" name="tiempo">
+		<br>
+		Lugar: <input type="text" name="lugar">
+		<br>
+	</form>
+
+
+
+</section>
